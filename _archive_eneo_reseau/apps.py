6ed2eed5ed4fffa4ac3f9000675e422591b4b357
SINSES <espoirsins@gmail.com>
2026-05-13 00:42:19 +0200

    chore: supprimer _archive_eneo_reseau — code legacy remplacé par sims_network

--- _archive_eneo_reseau/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EneoReseauConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eneo_reseau'
-    verbose_name = 'ENEO – Réseau & Incidents'
